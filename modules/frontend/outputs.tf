a4f83bd9816a6f247d0b75f1a36edada269dc675
Bob Yu <bobyu@google.com>
2025-12-19 05:40:29 +0000

    feat: add forwarding rule outputs

--- modules/frontend/outputs.tf
+++ modules/frontend/outputs.tf
@@ -84,7 +84,7 @@ output "apphub_service_uri" {
   description = "A list of all App Hub service URIs, including HTTP, HTTPS, and IPv6 versions."
 }
 
-output "forwarding_rule" {
+output "forwarding_rules" {
   description = "A list of forwarding rules created by this module."
   value = concat(
     local.create_http_forward ? [
